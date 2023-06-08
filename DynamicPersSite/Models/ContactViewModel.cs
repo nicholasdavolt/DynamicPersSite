2d17336215241c58823b91eaf765fa5fddd56f9e
Nicholas Davolt <nicholadavolt@outlook.com>
2023-06-08 16:41:04 -0500

    Completed Contact Form conversion and email functionality.

---
+++ DynamicPersSite/Models/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DynamicPersSite.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.EmailAddress)]//Certain formatting is expected (@ symbol, .com, etc)
+        public string Email { get; set; }
+        [Required(ErrorMessage = "* Required")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.MultilineText)] //Makes the textbox for this field bigger.
+        public string Message { get; set; }
+    }
+}
